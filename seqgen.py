d22e30efd5b537d8d8eaa0d1466652c48b8e246d
scivision <scivision@users.noreply.github.com>
2016-11-09 19:51:50 -0500

    demo

---
+++ seqgen.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from pathlib import Path
+from numpy import arange,fromfile,tile
+
+fn1 = '/tmp/tx0-255.bin'
+fn2 = '/tmp/rx0-255.bin'
+
+#%% transmit code write
+A = tile(arange(2**8, dtype='uint8'),8)
+
+fn1 = Path(fn1).expanduser()
+A.tofile(str(fn1),'')
+#%% receive code comparison
+B = fromfile(fn2,'uint8',sep='')
+
+match = A==B
+
+if not match.all():
+    fail = (~match).sum()
+    print('{} bytes mismatch'.format(fail))
+
