480dd465b4d080f6b60865853547560aa2ce6991
scivision <scivision@users.noreply.github.com>
2016-11-11 16:33:55 -0500

    add public domain kitten example

--- seqgen.py
+++ seqgen.py
@@ -6,7 +6,7 @@ fn1 = '/tmp/tx0-255.bin'
 fn2 = '/tmp/rx0-255.bin'
 
 #%% transmit code write
-A = tile(arange(2**8, dtype='uint8'),8)
+A = arange(2**8, dtype='uint8')
 
 fn1 = Path(fn1).expanduser()
 A.tofile(str(fn1),'')
@@ -15,7 +15,7 @@ B = fromfile(fn2,'uint8',sep='')
 
 match = A==B
 
-if not match.all():
-    fail = (~match).sum()
-    print('{} bytes mismatch'.format(fail))
+#if not match.all():
+#    fail = (~match).sum()
+#    print('{} bytes mismatch'.format(fail))
 
