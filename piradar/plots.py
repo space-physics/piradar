cb60876dc793050166dc955713420e88df029cb2
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-03-10 17:32:57 -0500

    reusable plotting spectrogram

--- piradar/plots.py
+++ piradar/plots.py
@@ -1,41 +1,73 @@
-from numpy import arange
+from datetime import datetime,timedelta
+from numpy import arange,log10
 from numpy.fft import fftshift
 from scipy.signal import spectrogram
 from matplotlib.pyplot import figure,subplots
-from matplotlib.colors import LogNorm
 
-def spec(sig,Fs):
-    f,t,Sxx = spectrogram(sig,fs=Fs)
+def spec(sig,Fs:int,flim=None,t0:datetime=None,ftick=None):
+    """
+    sig: signal to analyze, Numpy ndarray
+    """
+    twin = 0.200 # time length of windows [sec.]
+    Nfft = int(Fs*twin)
+    Nol = int(Fs*twin/2)
+    
+    f,t,Sxx = spectrogram(sig,fs=Fs,
+                          nfft=Nfft,
+                          nperseg=Nfft,
+                          noverlap=Nol) # [V**2/Hz]
+    if t0:
+        t = [t0 + timedelta(seconds=T) for T in t]
 
     f = fftshift(f)
-    Snorm = fftshift(Sxx/Sxx.max(),axes=0)
+    Snorm = fftshift(Sxx/Sxx.max(),axes=0) + 1e-10
 
     fg,axs = subplots(2,1)
-    fg.suptitle('$f_s$={} Hz'.format(Fs),y=0.99)
+    ttxt = f'$f_s$={Fs} Hz  Nfft {Nfft}  '
+    if t0:
+        ttxt += datetime.strftime(t0,'%Y-%m-%d')
+    fg.suptitle(ttxt, y=0.99)
 
     ax = axs[0]
-    h=ax.pcolormesh(t, f, Snorm,
-                    cmap='viridis',norm=LogNorm(),vmin=1e-4)
-    fg.colorbar(h,ax=ax).set_label('amplitude (normalized)')
+    h=ax.pcolormesh(t, f, 10*log10(Snorm),vmin=-40)
+    fg.colorbar(h,ax=ax).set_label('PSD (dB)')
     ax.set_ylabel('frequency [Hz]')
-    ax.set_xlabel('time [sec]')
+    ax.set_xlabel('time')
     ax.set_title('Spectrogram')
-    ax.autoscale(True,'both',True)
+    ax.autoscale(True,'both',tight=True)
+    if flim:
+        ax.set_ylim(flim)
+    if ftick is not None:
+        for ft in ftick:
+            ax.axhline(ft,color='red',linestyle='--')
 #%%
     ax=axs[1]
 
     Savg = Snorm.mean(axis=1)
     Savg /= Savg.max()
-
-    ax.semilogy(f,Savg)
-    ax.set_ylabel('amplitude (normalized)')
+    
+    if t0:
+        ts = (datetime.strftime(t[0],'%H:%M:%S'),datetime.strftime(t[-1],'%H:%M:%S'))
+    else:
+        ts = (t[0],t[1])
+    ttxt = f'time-averaged spectrum {ts[0]}..{ts[1]}'
+    
+    ax.plot(f,10*log10(Savg))
+    ax.set_ylabel('PSD (dB)')
     ax.set_xlabel('frequency [Hz]')
-    ax.set_ylim(1e-4,None)
-    ax.set_title('time-averaged spectrum')
+    ax.set_ylim(-40,None)
+    ax.set_title(ttxt)
     ax.autoscale(True,'x',True)
+    if flim:
+        ax.set_xlim(flim)
+    if ftick is not None:
+        for ft in ftick:
+            ax.axvline(ft,color='red',linestyle='--')
 
 
     fg.tight_layout()
+    
+    return f,t,Sxx
 
 def constellation_diagram(sig):
     ax = figure().gca()
