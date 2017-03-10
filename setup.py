02aaa1b838da72fda7057b19e6e954b6b4048292
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-03-09 22:28:24 -0500

    simple-setup

--- setup.py
+++ setup.py
@@ -1,29 +1,12 @@
 #!/usr/bin/env python
 from setuptools import setup
-import subprocess,sys,os
 
-ok = False
-try:
-    import conda.cli
-    conda.cli.main('install','--file','requirements.txt')
-except Exception: #system Python
-
-    with open('requirements.txt', 'r') as f:
-        req = f.read().split('\n')
-        aptreq = [os.path.basename(sys.executable)+'-'+r for r in req if r]
-
-    try:
-        cmd = ['sudo','apt-get','install'] + aptreq
-        print(' '.join(cmd))
-        subprocess.check_call(cmd)
-        ok=True
-    except Exception:
-        pass
-#%%
+req=['nose','numpy','scipy']
 
 setup(name='piradar',
-      packages=['piradar']
+      packages=['piradar'],
+      author='Michael Hirsch, Ph.D.',
+      description='HF radar for ionosphere using Red Pitaya for RF and Raspberry Pi coprocessor',
+      install_requires=req,
 	  )
 
-if not ok:
-    print('You will need {}'.format(req))
