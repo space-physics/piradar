af380d6b2fc4f1523272c4b37376bd7d52370e0a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-03-23 16:07:28 -0400

    init

--- PlotSpectrum.py
+++ PlotSpectrum.py
@@ -3,14 +3,29 @@
 Simply plots average power spectrum of a GNU Radio received file.
 Must know what sample rate of file was.
 """
-
+from pathlib import Path
 from numpy import fromfile
+from matplotlib.pyplot import show
 #
 from piradar.plots import spec
 
-fn = 'output/out.bin'
-fs = 1000000 # [Hz] a priori
+def loadspec(fn,fs):
 
-dat = fromfile(fn,'complex64')
+    fn = Path(fn).expanduser()
 
-spec(dat,fs)
+    dat = fromfile(str(fn),'complex64')
+
+    spec(dat.real,fs)
+    
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    p = ArgumentParser()
+    p.add_argument('fn',help='.bin file to process')
+    p.add_argument('fs',help='sample rate of .bin file [Hz]',type=int)
+    p = p.parse_args()
+
+    dat = loadspec(p.fn,p.fs)
+
+    show()
