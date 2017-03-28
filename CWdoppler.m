bba7e6da7d8c14c511264cbd86f168d622e86759
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-03-28 15:13:46 -0400

    zeropad

--- CWdoppler.m
+++ CWdoppler.m
@@ -4,7 +4,7 @@ fc = 15e3; %[Hz]
 Ac = 0.5;
 
 fb = fc + 3; %[Hz]
-Ab = 0.5;
+Ab = 0.1;
 
 fs = 100e3; %[Hz]
 t = 0:1/fs:1-1/fs;
@@ -26,18 +26,19 @@ try % for GNU Octave
 pkg load signal
 end
 
-dt = 0.35; %seconds between time steps to plot (arbitrary)
+dt = 0.45; %seconds between time steps to plot (arbitrary)
 dtw = 2*dt; % seconds to window
 tstep = ceil(dt*fs);
 wind = ceil(dtw*fs);
-Nfft = 2^nextpow2(wind);
-
+Nfft = 2^nextpow2(200*wind);
+disp(['Nfft: ',int2str(Nfft)])
+if 0
 figure(2);clf(2)
 specgram(dat,Nfft,fs,wind,wind-tstep);
-
 colorbar
 ylim([14990,15010])
+end
 %% periodogram
 figure(3); clf(3)
-pwelch(dat,wind,0.5,Nfft,fs)
+pwelch(dat,wind,0.5,Nfft,fs)%,[],'db')
 xlim([14990,15010])
