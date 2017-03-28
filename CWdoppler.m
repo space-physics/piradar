54c518f1f9d535c0d692e721e2816a48e824dd4d
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-03-28 14:56:57 -0400

    plot

--- CWdoppler.m
+++ CWdoppler.m
@@ -9,4 +9,26 @@ t = 0:1/fs:1-1/fs;
 xc = 0.5*sin(2*pi*fc*t);
 yc = 0.1*sin(2*pi*fb*t);
 
-plot(t,xc+yc)
+
+dat = xc+yc;
+%% Time
+figure(1); clf(1)
+plot(t,dat)
+xlabel('time [sec]')
+ylabel('amplitude')
+
+%% spectrogram
+try % for GNU Octave
+pkg load signal
+end
+
+dt = 0.35; %seconds between time steps to plot (arbitrary)
+dtw = 2*dt; % seconds to window
+tstep = ceil(dt*fs);
+wind = ceil(dtw*fs);
+
+figure
+specgram(dat,2^nextpow2(wind),fs,wind,wind-tstep);
+
+colorbar
+ylim([14990,15010])
