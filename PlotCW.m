e2fe6bb0876188d314eb2fa4122343a13552e80a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-04-09 01:34:58 -0400

    proper reading of complex data

--- PlotCW.m
+++ PlotCW.m
@@ -2,10 +2,12 @@
 % of course, these recordings are after DDC.
 % Issue with CW Nulling appears to include that recordings weren't coherent, frequency drifted, etc. from BG recording to target recording.
 % Michael Hirsch, Ph.D.
-function PlotCW()
+function PlotCW(varargin)
  try % for GNU Octave
   pkg load signal
  end
+
+ addpath('matlab/')
 %% user parameters
 
 %Anull = 1.6;
@@ -14,36 +16,30 @@ function PlotCW()
 %phi=-0.37;
 
 fs = 100000; % Hz, a priori
-%i = 1:500000;
-i = 500000:1100000; % beginning of signal times
+i = [1,10000];
+%i = [500000,1100000]; % beginning,end of signal times
 %i = 500000:500100; % manual nulling view
 
 %fnbg = 'data/cw_RX_nothing_new.bin';
-fn = 'data/cw_RX_air_2wire.bin';
-
-[~,name,ext] = fileparts(fn);
-t = i(1)/fs:1/fs:i(end)/fs;
 %% load data
-%fid = fopen(fnbg,'r');
-%bg = fread(fid,'float32=>float32');
-%fclose(fid);
-
-fid = fopen(fn,'r');
-sig = fread(fid,'float32=>float32');
-fclose(fid);
+fn = varargin{1};
+[~,name,ext] = fileparts(fn);
+sig = read_complex_binary(fn,i(2)-i(1),i(1));
+Ns = size(sig,2);
+t = i(1)/fs:1/fs:Ns/fs;
 %% process
 % extract wanted time segment of signal (so don't always have to work with entire file)
-%bg = bg(i); 
+%bg = bg(i);
 %ibg=10000;
 %bg = sig(ibg:length(t)+ibg-1);
-sig = sig(i);
+%sig = sig(i);
 
 % attempt manual CW nulling -- not working, need true coherent simultaneous sample which we don't have.
 %zbg = fft(bg);
 %N = length(zbg);
 %for k = 1:N
 %  w = 2*pi/N*(k-1);
-%  sbg(k) = zbg(k)*exp(-1j*w*phi);  
+%  sbg(k) = zbg(k)*exp(-1j*w*phi);
 %end
 %bg = real(ifft(sbg))';
 %sigsub = sig - Anull*bg;
@@ -52,7 +48,7 @@ sig = sig(i);
 
 radarplot(sig,t,[name,ext],fs)
 
-end 
+end
 
 function radarplot(sig,t,name,fs)
 
@@ -87,10 +83,10 @@ if 0
   dt = 0.1; %seconds between time steps to plot (arbitrary)
   dtw = 2*dt; % seconds to window
   tstep = ceil(dt*fs);  wind = ceil(dtw*fs);
-  
+
   figure(3),clf(3)
   specgram(sig, 2^nextpow2(wind),fs,wind,wind-tstep);
-  
+
   colorbar
   ylim([14990,15010])
 end
---
+++ matlab/read_complex_binary.m
@@ -0,0 +1,49 @@
+% https://github.com/gnuradio/gnuradio/blob/master/gr-utils/octave/read_complex_binary.m
+%
+% Copyright 2001 Free Software Foundation, Inc.
+%
+% This file is part of GNU Radio
+%
+% GNU Radio is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 3, or (at your option)
+% any later version.
+%
+% GNU Radio is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with GNU Radio; see the file COPYING.  If not, write to
+% the Free Software Foundation, Inc., 51 Franklin Street,
+% Boston, MA 02110-1301, USA.
+%
+
+function v = read_complex_binary (filename, count,start)
+% data = read_complex_binary (filename, [count])
+%
+%  open filename and return the contents as a column vector,
+%  treating them as 32 bit complex numbers
+%
+
+ narginchk (1,3)
+ 
+  if nargin < 2
+    count = Inf;
+  end
+  if nargin < 3
+    start = 0;
+  end
+
+  f = fopen(filename, 'rb');
+  if (f < 0)
+    error([filename,' not found'])
+  else
+    fseek(f,(start-1)*4*2,'bof');
+    t = fread (f, [count, 2], 'float');
+    fclose (f);
+    v = t(:,1) + t(:,2)*1i;
+    [r, c] = size (v);
+    v = reshape (v, c, r);
+  end
