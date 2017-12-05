17afb3cbe0db98de02d0376a1997a896ca72fe0a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-12-05 02:21:46 -0500

    TODO window, sim delay

--- StreamingChirpRX.py
+++ StreamingChirpRX.py
@@ -14,10 +14,13 @@ from piradar.plots import plotxcor
 UP = 125
 DOWN = 12
 LSAMP = 8  # 8 bytes per single precision complex64
+Nsim = 10000 # number of simulated pulses
 
 
 def procchunk(rx, tx, P:dict):
-    rx = scipy.signal.resample_poly(rx, UP, DOWN)
+    if P['rxfn'] is not None:
+        rx = scipy.signal.resample_poly(rx, UP, DOWN)
+
     fs = P['txfs']
 # %% resamples parameters
     NrxPRI = int(fs * P['pri']) # Number of RX samples per PRI (resampled)
@@ -41,13 +44,20 @@ if __name__ == '__main__':
     p.add_argument('rxfn',help='receive data filename')
     p.add_argument('rxfs',help='receiver sample freq.',type=float)
     p.add_argument('-txfn',help='transmit chirp sample',default='~/data/eclipse/txchirp.bin')
-    p.add_argument('-txfs',help='transmit sample freq.',type=float,default=2e6)
-    p.add_argument('-pri',help='pulse repetition interval [sec]',type=float,default=3.75e-3)
-    p.add_argument('-N','--Nchirp',help='number of chirps to chunk',type=int,default=1000)
+    p.add_argument('-txfs',help='transmit sample freq.',type=float, default=2e6)
+    p.add_argument('-pri',help='pulse repetition interval [sec]',type=float, default=3.75e-3)
+    p.add_argument('-N','--Nchirp',help='number of chirps to chunk',type=int, default=1000)
     p = p.parse_args()
 
-    P = {'rxfn':Path(p.rxfn).expanduser(),
-         'rxfs':p.rxfs,
+    if p.rxfn == 'sim':
+        rxfn = None
+        rxfs = p.txfs
+    else:
+        rxfn = Path(p.rxfn).expanduser()
+        rxfs = p.rxfs
+
+    P = {'rxfn':rxfn,
+         'rxfs':rxfs,
          'txfn':Path(p.txfn).expanduser(),
          'txfs':p.txfs,
          'pri':p.pri,
@@ -57,14 +67,25 @@ if __name__ == '__main__':
     tx = loadbin(P['txfn'], P['txfs'], tlim=(0,P['pri']))
     print(f'Using {P["pri"]*1000} ms PRI and {P["Nchirp"]} pulses incoherently integrated')
 # %%
-    Nsampfile = P['rxfn'].stat().st_size//LSAMP
-    Tfile = Nsampfile / P['rxfs']
-    print(P['rxfn'], f'is {Tfile:0.1f} seconds long')
+    NrxPRI = int(P['pri']*rxfs)  # samples in a PRI
 
-    NrxPRI = int(p.pri*p.rxfs)  # samples in a PRI
+    if P['rxfn'] is not None:
+        Nsampfile = P['rxfn'].stat().st_size//LSAMP
+        Tfile = Nsampfile / P['rxfs']
+        print(P['rxfn'], f'is {Tfile:0.1f} seconds long')
+        isamp = range(0, Nsampfile, NrxPRI*P['Nchirp'])
+    else:
+        print('simulation')
+        isamp = range(0, Nsim*NrxPRI*P['Nchirp'], NrxPRI*P['Nchirp'])
 
-    isamp = range(0, Nsampfile, NrxPRI*P['Nchirp'])
+
 
     for i,j in zip(isamp,isamp[1:]):
-        rx = loadbin(P['rxfn'], P['rxfs'], isamp=(i, j))
+        if P['rxfn'] is None:
+            rx = 0.05*tx + 0.1*tx.max()*(np.random.randn(P['Nchirp'],tx.size)
+                                         + 1j*np.random.randn(P['Nchirp'],tx.size))
+            rx = rx.ravel()
+        else:
+            rx = loadbin(P['rxfn'], P['rxfs'], isamp=(i, j))
+
         procchunk(rx, tx, P)
