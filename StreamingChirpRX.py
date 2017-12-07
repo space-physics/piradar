5c629b34c035d3073622ddf109c9103cfd96b31d
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-12-07 15:20:43 -0500

    functionalize

--- StreamingChirpRX.py
+++ StreamingChirpRX.py
@@ -5,6 +5,7 @@ For now, choose to upsample RX to TX.
 
 ./StreamingChirpRX.py ~/data/eclipse/zenodo/radar2017-08-22T00-52-40_3.62MHz.bin 192e3 -t0 1800 -o /tmp/test.h5
 
+./StreamingChirpRX.py sim
 """
 import sys
 from pathlib import Path
@@ -19,6 +20,58 @@ from piradar.plots import plotxcor,plotraw
 #
 LSAMP = 8  # 8 bytes per single precision complex64
 Nsim = 10000 # number of simulated pulses
+
+def analyze(P:dict):
+
+    tx = loadbin(P['txfn'], P['txfs'], tlim=(0,P['tm']))
+    if P['verbose']:
+        plotraw(tx, None, P['txfs'])
+    print(f'Using {P["pri"]*1000} ms PRI for {P["tm"]*1e6} us chirp, with {P["Nchirp"]} pulses incoherently integrated')
+# %%
+    NrxPRI = int(P['pri']*rxfs)  # samples in a PRI
+    Lrx = NrxPRI*P['Nchirp']
+
+    if P['rxfn'] is not None:
+        Nsampfile = P['rxfn'].stat().st_size//LSAMP
+        Tfile = Nsampfile / P['rxfs']
+        print(P['rxfn'], f'is {Tfile:0.1f} seconds long')
+        isamp = range(0, Nsampfile, Lrx)
+
+    else:
+        print('simulation')
+        isamp = range(0, Nsim*Lrx, Lrx)
+
+    t = np.array(isamp) / P['txfs']  # elapsed time seconds
+    if P['t0'] is not None:
+        t += P['t0']
+# %%
+    if P['outfn']:
+        print('writing',outfn)
+        with h5py.File(outfn,'w') as f:
+            f.create_dataset('Rxy',shape=(t.size, int(NrxPRI*P['resample'])),
+                             dtype=np.complex128,chunks=True,compression='gzip')
+            f['t'] = t
+            f['lags'] = np.arange(-NrxPRI//2,NrxPRI//2)
+            f['t0'] = P['t0']
+            f['cmd'] = ' '.join(sys.argv)
+            f['fs'] = P['txfs']
+
+
+    for k,(i,j) in enumerate(zip(isamp,isamp[1:])):
+        if P['rxfn'] is None:
+            rx = 0.05*tx + 0.1*tx.max()*(np.random.randn(P['Nchirp'],tx.size)
+                                         + 1j*np.random.randn(P['Nchirp'],tx.size))
+            rx = rx.ravel()
+        else:
+            rx = loadbin(P['rxfn'], P['rxfs'], tlim=P['t0'], isamp=(i, j))
+
+        lags = procchunk(rx, tx, P)
+
+        if outfn:
+            with h5py.File(outfn,'a') as f:
+                f['Rxy'][k,:] = lags
+
+        print(f'processing t={t[k]:.2f} sec., {t[k]/Tfile*100:.3f} % complete.\r',end="")
 
 
 def procchunk(rx, tx, P:dict):
@@ -48,11 +101,23 @@ def procchunk(rx, tx, P:dict):
     return Rxy
 
 
+def loadplot(P:dict):
+    """load already processed correlation data and plot"""
+    with h5py.File(P['rxfn'], 'r') as f:
+        fs = f['fs'].value
+        t = f['t'][:]
+        lags = f['lags'][:]
+        t0 = f['t0'].value
+        Rxy = f['Rxy']  # too big for RAM
+
+
+
+
 if __name__ == '__main__':
     from argparse import ArgumentParser
     p = ArgumentParser()
     p.add_argument('rxfn',help='receive data filename')
-    p.add_argument('rxfs',help='receiver sample freq.',type=float)
+    p.add_argument('rxfs',help='receiver sample freq.',nargs='?',type=float)
     p.add_argument('-txfn',help='transmit chirp sample',default='~/data/eclipse/txchirp.bin')
     p.add_argument('-txfs',help='transmit sample freq.',type=float, default=2e6)
     p.add_argument('-pri',help='pulse repetition interval [sec]',type=float, default=3.75e-3)
@@ -63,6 +128,7 @@ if __name__ == '__main__':
     p.add_argument('-t0',help='skip ahead to start at this elapsed time',type=float)
     p = p.parse_args()
 
+# %% setup analysis type
     outfn = Path(p.outfn).expanduser() if p.outfn else None
 
     if p.rxfn == 'sim':
@@ -80,57 +146,14 @@ if __name__ == '__main__':
          'Nchirp':p.Nchirp,
          'tm':p.tm,
          't0':p.t0,
+         'outfn':outfn,
          'verbose':p.verbose,
          }
 
     if p.txfs and p.rxfs:
         P['resample'] = fractions.Fraction(p.txfs/p.rxfs).limit_denominator()
 
-    tx = loadbin(P['txfn'], P['txfs'], tlim=(0,P['tm']))
-    if P['verbose']:
-        plotraw(tx, None, P['txfs'])
-    print(f'Using {P["pri"]*1000} ms PRI for {P["tm"]*1e6} us chirp, with {P["Nchirp"]} pulses incoherently integrated')
-# %%
-    NrxPRI = int(P['pri']*rxfs)  # samples in a PRI
-    Lrx = NrxPRI*P['Nchirp']
-
-    if P['rxfn'] is not None:
-        Nsampfile = P['rxfn'].stat().st_size//LSAMP
-        Tfile = Nsampfile / P['rxfs']
-        print(P['rxfn'], f'is {Tfile:0.1f} seconds long')
-        isamp = range(0, Nsampfile, Lrx)
-
-    else:
-        print('simulation')
-        isamp = range(0, Nsim*Lrx, Lrx)
-
-    t = np.array(isamp) / P['txfs']  # elapsed time seconds
-    if P['t0'] is not None:
-        t += P['t0']
-# %%
-    if outfn:
-        print('writing',outfn)
-        with h5py.File(outfn,'w') as f:
-            f.create_dataset('Rxy',shape=(t.size, int(NrxPRI*P['resample'])),
-                             dtype=np.complex128,chunks=True,compression='gzip')
-            f['t'] = t
-            f['lags'] = np.arange(-NrxPRI//2,NrxPRI//2)
-            f['t0'] = P['t0']
-            f['cmd'] = ' '.join(sys.argv)
-
-
-    for k,(i,j) in enumerate(zip(isamp,isamp[1:])):
-        if P['rxfn'] is None:
-            rx = 0.05*tx + 0.1*tx.max()*(np.random.randn(P['Nchirp'],tx.size)
-                                         + 1j*np.random.randn(P['Nchirp'],tx.size))
-            rx = rx.ravel()
-        else:
-            rx = loadbin(P['rxfn'], P['rxfs'], tlim=P['t0'], isamp=(i, j))
-
-        lags = procchunk(rx, tx, P)
-
-        if outfn:
-            with h5py.File(outfn,'a') as f:
-                f['Rxy'][k,:] = lags
-
-        print(f'processing t={t[k]:.2f} sec., {t[k]/Tfile*100:.3f} % complete.\r',end="")
+    if P['rxfn'].suffix == '.bin':
+        analyze(P)
+    elif P['rxfn'].suffix == '.h5':
+        loadplot(P)
